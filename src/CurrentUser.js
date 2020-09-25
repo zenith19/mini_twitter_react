d06383d188604b8cd0805479f6b9e0b454495a11
zenithit <knotholaze@gmail.com>
2020-09-25 09:55:56 +0600

    move files 2

--- src/CurrentUser.js
+++ src/CurrentUser.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "./CurrentUser.css";
+import "./style/CurrentUser.css";
 
 const CurrentUser = () => {
   const [currentUser, setCurrentUser] = useState({});
